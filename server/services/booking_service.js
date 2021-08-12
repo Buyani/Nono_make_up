b0fc2c9097879c882ff82b83622f64b0cfa55fb4
Buyani <buyanimhlongo@gmail.com>
2021-08-12 12:43:08 +0200

    admin layout updates

--- server/services/booking_service.js
+++ server/services/booking_service.js
@@ -1,7 +1,13 @@
 const axios=require("axios");
 
 exports.booking_home=(req,res)=>{
-    res.status(200).send({data:"booking home from service"});
+    axios.get('http://localhost:3000/api/bookings')
+    .then(function(response){
+        res.render('bookings', { bookings : response.data });
+    })
+    .catch(err =>{
+        res.send(err);
+    })
 }
 
 exports.create_booking = (req, res) =>{
